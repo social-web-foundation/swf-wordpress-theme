ef4d082542c0b75b0091d8f9ce876af71e9fae20
Evan Prodromou <evan@openearth.org>
2024-09-21 12:19:48 -0400

    feat: take out article-copy div
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- index.php
+++ index.php
@@ -4,14 +4,12 @@
 
 <h1><?php wp_title(''); ?></h1>
 
-<div class="article-copy">
-  <?php
-      wp_reset_query(); // necessary to reset query
-      while ( have_posts() ) : the_post();
-          the_content();
-      endwhile; // End of the loop.
-  ?>
-</div>
+<?php
+    wp_reset_query(); // necessary to reset query
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile; // End of the loop.
+?>
 
 </div>
 
