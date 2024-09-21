82463872636ec265a0f4fa69e7cf86bd989002e7
Evan Prodromou <evan@openearth.org>
2024-09-21 09:43:53 -0400

    feat: fixup header, index
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- header.php
+++ header.php
@@ -12,25 +12,27 @@
 
 <body <?php body_class('homepage'); ?>>
 
-<header class="top-bar" id="top-bar-homepage">
-    <div class="container">
-        <div class="logo">
-            <a href="<?php echo home_url(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/logo-5.png" id="logo-image" alt="<?php bloginfo('name'); ?> Logo">
-                <?php bloginfo('name'); ?>
-            </a>
+
+    <header class="top-bar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/logo-5.png" id="logo-image" alt="<?php bloginfo('name'); ?> Logo">
+                    <?php bloginfo('name'); ?>
+                </a>
+            </div>
+            <nav>
+
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'primary',
+                        'container' => false,
+                        'menu_class' => '',
+                    ) );
+                ?>
+            </nav>
+
+            <button class="menu-button">Menu</button>
+
         </div>
-
-        <nav>
-            <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'primary',
-                    'container' => false,
-                    'menu_class' => '',
-                ) );
-            ?>
-        </nav>
-
-        <button class="menu-button">Menu</button>
-    </div>
-</header>
+    </header>
--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php get_header(); ?>
 
-<div id="hero">
-    <header>
-        <h1><?php bloginfo('name'); ?></h1>
-    </header>
-    <img src="<?php echo get_template_directory_uri(); ?>/images/small-circles-solid.svg" id="hero-image" alt="">
-    <p class="fade-in-element">Welcome to <?php bloginfo('name'); ?>. This is a placeholder for your site's main content.</p>
+<div class="internal-content">
+
+<h1><?php wp_title('|', true, 'right'); ?></h1>
+
+<div class="article-copy">
+  <?php
+      wp_reset_query(); // necessary to reset query
+      while ( have_posts() ) : the_post();
+          the_content();
+      endwhile; // End of the loop.
+  ?>
 </div>
 
-<div class="content">
-    <!-- Main content area, WordPress loop will go here -->
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <div><?php the_content(); ?></div>
-    <?php endwhile; else : ?>
-        <p><?php _e( 'Sorry, no content available.' ); ?></p>
-    <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
